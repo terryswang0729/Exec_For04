032588532138e8a91cb82fae906cf3919c8204ff
Terry <Terry@DESKTOP-ETETAAM>
2022-10-19 23:50:40 +0800

    加入專案檔案。

---
+++ Exec_For04/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exec_For04
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			for (int i=1; i <= 20; i++)
+			{
+				if (i % 15 == 0)
+				{
+					Console.WriteLine("foobar");
+				}
+				else
+				{
+					if (i % 5 == 0)
+					{
+						Console.WriteLine("bar");
+					}
+					else if (i % 3 == 0)
+					{
+						Console.WriteLine("foo");
+					}
+					else
+					{
+						Console.WriteLine(i);
+					}
+				}
+			}
+		}
+	}
+}
